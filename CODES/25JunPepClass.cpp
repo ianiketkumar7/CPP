eb9392bb65111140c0f141b72980637c20e92f54
ianiketkumar7 <aniketkumar.nikeman@gmail.com>
2025-06-25 13:28:29 +0530

    sync

--- CODES/25JunPepClass.cpp
+++ CODES/25JunPepClass.cpp
@@ -92,11 +92,11 @@ int main() {
     // a = b;
     // b = temp;
     // cout << "Value of a: " << a << " & " << "Value of b: " << b << " after swaping." << endl;
-    a = b + 10;
-    a = a - 10;
-    b = a + 10;
-    b = b - 10;
-    cout << "Value of a: " << a << " & " << "Value of b: " << b << " after swaping." << endl;
+    // a = b + 10;
+    // a = a - 10;
+    // b = a + 10;
+    // b = b - 10;
+    // cout << "Value of a: " << a << " & " << "Value of b: " << b << " after swaping." << endl;
 
 
 }
