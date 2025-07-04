e95a219934e3dabb9908de0d478116b872880e1a
ianiketkumar7 <aniketkumar.nikeman@gmail.com>
2025-07-04 11:35:25 +0530

    update

--- CODES/tempCodeRunnerFile.cpp
+++ CODES/tempCodeRunnerFile.cpp
@@ -1,5 +1,6 @@
-{
-        //     if(j == 0 || j == n - 1) {
-        //         cout << "*" << " ";
-        //     }
-        // }
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 10, b = 20;
+    cout << 10%23;
+}
